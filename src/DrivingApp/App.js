a3b67c01d0fcc14957396904e7b5d2c91098d965
IamMatt117 <81575818+IamMatt117@users.noreply.github.com>
2023-09-28 20:45:53 +0100

    Created a homepage

---
+++ src/DrivingApp/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+
+export default function App() {
+  const [colourChange, setColourChange] = useState(true)
+  return (
+    <View style={colourChange ? styles.container : styles.change}>
+      <Text style={colourChange ? styles.black : styles.white} onPress={() => setColourChange(!colourChange)}>Driving App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  change: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  white: {
+    color: 'white',
+  },
+  black: {
+    color: 'black',
+  },
+});
+
+
